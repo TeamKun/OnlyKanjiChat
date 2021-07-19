ac14ff1c9154e27e0f8c517b73390d31f363dd54
dai-1103 <bosatsunumnum@gmail.com>
2021-07-20 06:57:23 +0900

    create project

---
+++ src/main/java/net/kunmc/llab/onlykanjichat/OnlyKanjiChat.java
@@ -0,0 +1,17 @@
+package net.kunmc.llab.onlykanjichat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OnlyKanjiChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
