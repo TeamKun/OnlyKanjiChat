66ffb509913dc2cc4fe1c447a75cf9500dd54793
dai-1103 <bosatsunumnum@gmail.com>
2021-07-20 09:08:04 +0900

    リファクタリング

--- src/main/java/net/kunmc/llab/onlykanjichat/event/SignChangeEvent.java
+++ src/main/java/net/kunmc/llab/onlykanjichat/event/SignChangeEvent.java
@@ -7,13 +7,21 @@ import org.bukkit.event.Listener;
 public class SignChangeEvent implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onSignChange(org.bukkit.event.block.SignChangeEvent event) {
-        // １行づつ処理
+
+        /** 行ごとの入力内容 */
         String[] lines = event.getLines();
 
+        /** 行カウンタ */
         int index = 0;
+
+        // １行づつ処理
         for (String line : lines) {
+            // 行から漢字のみを抽出
             String extractResult = KanjiExtractor.extractKanji(line);
+
+            // 看板にセット
             event.setLine(index ,extractResult);
+
             index ++;
         }
     }
